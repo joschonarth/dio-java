5c2a31c2a1bc1a05adb65d5b20fab1aef6cc3b58
joschonarth <joschonarth@gmail.com>
2024-06-30 11:14:41 -0300

    enums

---
+++ java-basico/conteudos/enums/src/SistemaIbge.java
@@ -0,0 +1,13 @@
+public class SistemaIbge {
+	public static void main(String[] args) {
+		//imprimindo os estados existentes no enum
+		for(EstadoBrasileiro uf: EstadoBrasileiro.values() ) {
+		   System.out.println(uf.getSigla() + "-" + uf.getNomeMaiusculo());
+		}
+		
+		//selecionando um estado a partir das opções disponíveis
+		EstadoBrasileiro ufSelecionado = EstadoBrasileiro.SAO_PAULO;
+		
+		System.out.println("O estado selecionado foi: " + ufSelecionado.getNome());
+	}
+}
