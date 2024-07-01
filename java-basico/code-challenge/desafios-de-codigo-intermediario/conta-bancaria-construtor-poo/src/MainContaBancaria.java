e1b704c726ce38c1b3029521999c733046ce611d
joschonarth <joschonarth@gmail.com>
2024-07-01 13:59:08 -0300

    desafio 3

---
+++ java-basico/code-challenge/desafios-de-codigo-intermediario/conta-bancaria-construtor-poo/src/MainContaBancaria.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class MainContaBancaria {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double saldoInicial = scanner.nextDouble();
+        
+        ContaBancaria conta = new ContaBancaria(saldoInicial);
+
+        // Lê a Entrada com o valor do deposito e aciona o método "depositar"
+        double valorDeposito = scanner.nextDouble();
+        conta.depositar(valorDeposito);
+
+        // Lê a Entrada com o valor do saque e aciona o método "sacar"
+        double valorSaque = scanner.nextDouble();
+        conta.sacar(valorSaque);
+
+        // Fechar o scanner para evitar vazamentos de recursos
+        scanner.close();
+    }
+}
+
+class ContaBancaria {
+    private double saldo;
+
+    public ContaBancaria(double saldoInicial) {
+        this.saldo = saldoInicial;
+    }
+
+    public void depositar(double valor) {
+        this.saldo += valor;
+        System.out.println("Deposito feito.");
+        imprimirSaldo();
+    }
+
+    public void sacar(double valor) {
+        if (valor > saldo) {
+            // Se for, imprime uma mensagem informando que o saque não pode ser realizado por saldo insuficiente:
+            System.out.println("Saldo insuficiente. Saque não realizado.");
+        } else {
+            // Caso contrário, realiza o saque subtraindo o valor do saldo da conta:
+            this.saldo -= valor;
+            // Imprime uma mensagem indicando que o saque foi feito:
+            System.out.println("Saque feito.");
+            imprimirSaldo();
+        }
+    }
+    
+    private void imprimirSaldo() {
+        System.out.printf("Saldo atual: %.1f\n", this.saldo);
+    }
+
+}
