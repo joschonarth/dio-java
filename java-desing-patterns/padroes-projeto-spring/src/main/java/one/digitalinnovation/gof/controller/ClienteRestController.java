62a84e197e3a6b799302136f374f61737e6f621e
joschonarth <joschonarth@gmail.com>
2024-07-06 13:58:51 -0300

    Update Design Patters com Spring REST API

--- java-desing-patterns/padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/controller/ClienteRestController.java
+++ java-desing-patterns/padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/controller/ClienteRestController.java
@@ -14,12 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import one.digitalinnovation.gof.model.Cliente;
 import one.digitalinnovation.gof.service.ClienteService;
 
-/**
- * Esse {@link RestController} representa nossa <b>Facade</b>, pois abstrai toda
- * a complexidade de integrações (Banco de Dados H2 e API do ViaCEP) em uma
- * interface simples e coesa (API REST).
- * 
- */
+
 @RestController
 @RequestMapping("clientes")
 public class ClienteRestController {
