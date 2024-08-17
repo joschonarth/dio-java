b9f02e7dc2fd36cb55f4c6c9987be853e0d86543
joschonarth <joschonarth@gmail.com>
2024-08-17 15:06:07 -0300

    Desafios de Código

---
+++ java-basico/code-challenges/desafios-de-codigo-intermediario/validando-endpoints/src/EndpointValidator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EndpointValidator {
+    public static String validateEndpoint(String endpoint) {
+        // TODO: Verifique se o endpoint fornecido segue o formato padrão de endpoint de API:
+        String regex = "^/api/\\w+(/\\w+)*$";
+
+        if (endpoint.matches(regex)) {
+            return "Endpoint valido.";
+        } else {
+            return "Endpoint invalido.";
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String endpoint = scanner.nextLine();
+        System.out.println(validateEndpoint(endpoint));
+    }
+}
